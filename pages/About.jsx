b3df9f7ef4663c619d2ecb9ffaf78ed6d2e45192
Gab Erves <gerves20@gmail.com>
2023-05-10 15:53:12 -0400

    finally workinggit add .

---
+++ pages/About.jsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+import React from "react";
+import '../app/globals.css';
+
+const About = () => {
+  return <div>
+  <Header/>
+  Welcome to Listomania, your ultimate destination for creating and managing virtual lists! Whether you're organizing your favorite movies, planning your dream vacation itinerary, or compiling a list of must-read books, Listomania provides the perfect platform to bring your lists to life. With our user-friendly interface, you can easily create, edit, and share your lists with friends, family, or the entire online community. Unleash your creativity, stay organized, and discover new possibilities as you dive into the world of Listomania, where your lists are only limited by your imagination.
+  </div>;
+};
+
+export default About;
---
+++ pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+import React from "react";
+import '../app/globals.css';
+
+const HomePage = () => {
+  return <div>
+  <Header/>
+Listomania Lists
+  </div>;
+};
+
+export default HomePage;

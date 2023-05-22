0173fef72d42806ef2f5d94d06e391eee3682c75
Savion Watson <72318904+SavionW-Works@users.noreply.github.com>
2023-05-21 23:49:56 -0400

    Last Polish...?
    -Prevented Edit from Showing up on non-owned lists

--- pages/Profile.jsx
+++ pages/Profile.jsx
@@ -22,6 +22,7 @@ import {
 const Profile = ({ user_id }) => {
   const [localUsername, setLocalUsername] = useState("");
   const [localLists, setLocalLists] = useState({});
+  const [localID, setLocalID] = useState("");
   const router = useRouter();
 
   useEffect(() => {
@@ -46,6 +47,17 @@ const Profile = ({ user_id }) => {
     fetchLists();
   }, [user_id]);
 
+  const idFetcher = async () => {
+    const hold = await getCurrentID();
+    if (hold) {
+      setLocalID(hold);
+    }
+  };
+
+  useEffect(() => {
+    idFetcher();
+  }, []);
+
   //Reroutes user to editing page, if its their list. key = list_id
   const checkListOwner = (key) => {
     if (ifOwnList(key, user_id)) {
@@ -64,7 +76,6 @@ const Profile = ({ user_id }) => {
     return `/user/${user_id}/list/${key}/edit`;
   };
 
-
   return (
     <>
       <LoggedInHeader />
@@ -72,23 +83,28 @@ const Profile = ({ user_id }) => {
       <h3 className="text-center text-5xl font-bold p-10">Current Lists</h3>
       {/* <p>{localLists}</p> */}
 
-     <div className="grid flex justify-center">
+      <div className="grid flex justify-center">
         {Object.entries(localLists).map(([key, value]) => {
           return (
-            <div className="container text-center justify-items-center py-3" key={key}>
+            <div
+              className="container text-center justify-items-center py-3"
+              key={key}
+            >
               <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <Link href={redirectToViewer(key)}>
                   <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     {value}
                   </div>
                 </Link>
-                <div className="padd2">
-                  <Link href={redirectToEdit(key)}>
-                    <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                      Edit
-                    </button>
-                  </Link>
-                </div>
+                {localID === user_id && (
+                  <div className="padd2">
+                    <Link href={redirectToEdit(key)}>
+                      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        Edit
+                      </button>
+                    </Link>
+                  </div>
+                )}
               </div>
             </div>
           );

8369981b044710f3f79f5e70951676147e430694
Gab Erves <gerves20@gmail.com>
2023-05-21 23:15:54 -0400

    testing

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,12 +15,12 @@ const Header = () => {
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-stone-500 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
+            <Link
               className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
               href="/HomePage"
             >
               <Image src={ListIcon} alt={'LinkBarge'} height="120" width="120" />
-            </a>
+            </Link>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -38,36 +38,36 @@ const Header = () => {
           >
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
-                <a
+                <Link
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                   href="/HomePage"
                 >
                   <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Home</span>
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
+                <Link
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                   href="/Register"
                 >
                   <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Register</span>
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
+                <Link
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                   href="/Login"
                 >
                   <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Login</span>
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
+                <Link
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                   href="/About"
                 >
                   <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">About</span>
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
--- components/LoggedInHeader.jsx
+++ components/LoggedInHeader.jsx
@@ -57,7 +57,7 @@ const LoggedInHeader = () => {
         <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-stone-500 mb-3">
           <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
             <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-              <a
+              <Link
                 className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
                 href="/HomePage"
               >
@@ -67,7 +67,7 @@ const LoggedInHeader = () => {
                   height="120"
                   width="120"
                 />
-              </a>
+              </Link>
               <button
                 className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
                 type="button"
@@ -94,32 +94,32 @@ const LoggedInHeader = () => {
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a
+                  <Link
                     className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                     href="/CreateList"
                   >
                     <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
                     <span className="ml-2">Create</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a
+                  <Link
                     className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                     href={`/user/${localID}`}
                   >
                     <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i>
                     <span className="ml-2">My Lists</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a
+                  <Link
                     className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                     href="/Login"
                     onClick={handleLogout}
                   >
                     <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
                     <span className="ml-2">Logout</span>
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
